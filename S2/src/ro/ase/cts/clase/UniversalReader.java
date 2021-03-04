93e9b7b0befe1908157998552509380f3f5f5772
Mete <meteabduraman@gmail.com>
2021-03-04 12:22:53 +0200

    Single Responsability and Dependency Inversion principles

---
+++ S2/src/ro/ase/cts/clase/UniversalReader.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.clase;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UniversalReader {
+
+	//TODO punct de legatura catre readere
+	
+}
